fb079cb709f6fa81393460de6106b35e6ca99c7d
mepavankumar15 <119803977+mepavankumar15@users.noreply.github.com>
2026-03-26 14:53:58 +0530

    actions in 07

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -4,4 +4,4 @@ NUM1=100
 NUM2=Sureh
 
 NUM3=("Hi" "Bie" "Hy") 
-echo "th values are : ${NUM3[@]}"
+echo "the values are : ${NUM3[@]}"
---
+++ 07-conditions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NUMBER=$1
+NUMBER2=$2
+
+if [ $NUMBER -gt 20 ]; then
+    echo "Given Number ${NUMBER} is greater 20"
+fi
+
+# else conditon
+if [ $NUMBER2 -gt 40 ]; then
+    echo "Given Number is ${NUMBER2} is greater than 40"
+else 
+    echo "it is less than 40"
+fi
+
+# else if or elif
+
+echo "enter the value"
+read STR
+
+if [ STR -eq "Suresh" ]; then
+    echo "He is Admin"
+elif [ STR -eq "Ramesh" ]; then
+    echo "He is User"
+else
+    echo "Not Allowed"
+fi
