7cc01f87935596428f4f4d6707a98e0d936ffa85
mepavankumar15 <119803977+mepavankumar15@users.noreply.github.com>
2026-03-26 20:07:10 +0530

    actions

--- 12-loops.sh
+++ 12-loops.sh
@@ -4,3 +4,26 @@ for i in {1..10}
 do
     echo $i
 done
+
+LOGS_FOLDER="/var/log/shell-script"
+LOG_FIL="/var/log/shell-script/$0.log"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "use this command in root user " | tee -a $LOG_FILE
+fi
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo "$2 ... SUCCESS" | tee -a $LOG_FILE
+    else 
+        echo "$2... FAILED" | tee -a $LOG_FILE
+    fi
+}
+
+for package in $@
+do
+    dnf install $package -y
+    VALIDATE $? "$package installation"
+done
