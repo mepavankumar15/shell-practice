9002aac2e1d2c6a890f4f1bee87ace96b2671c37
mepavankumar15 <119803977+mepavankumar15@users.noreply.github.com>
2026-03-27 11:23:35 +0530

    actions

--- 16-trap.sh
+++ 16-trap.sh
@@ -6,5 +6,5 @@ trap 'echo "there is an error in $LINENO , Command: $BASH_COMMAND"' ERR
 
 echo "Hello World"
 echo "I am learning Shell"
-echo "printing error here"
+echoo "printing error here"
 echo "No error in this"
